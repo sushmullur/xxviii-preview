c472c26caa18256543cd0914a95dff6db6932698
sushpicious <2011658@apps.nsd.org>
2023-04-25 19:58:48 -0700

    Adjusted title length

--- src/components/AlbumTitle.js
+++ src/components/AlbumTitle.js
@@ -3,7 +3,7 @@ import styles from './AlbumTitle.module.css';
 export const AlbumTitle = () => {
     return (
         <div className={styles.wrapper}>
-            <h1>XXVIII: The Album</h1>
+            <h1>XXVIII</h1>
         </div>
     );
 }
